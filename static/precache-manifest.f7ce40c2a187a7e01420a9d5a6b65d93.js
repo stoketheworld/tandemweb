19fbe76a65ca6f555aa0a8f66fb395874d5b9af9
Mark <cybersyn@gmail.com>
2020-10-17 15:37:59 -0700

    changed node_env to production for this build.

---
+++ static/precache-manifest.f7ce40c2a187a7e01420a9d5a6b65d93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869ab7bc86b8a58c1e087a134f865872",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa115d08712a30b68602",
+    "url": "/static/css/2.d2976a7d.chunk.css"
+  },
+  {
+    "revision": "82f7ece268cd202ce9bc",
+    "url": "/static/css/main.b5000694.chunk.css"
+  },
+  {
+    "revision": "aa115d08712a30b68602",
+    "url": "/static/js/2.2eb01a7b.chunk.js"
+  },
+  {
+    "revision": "c2f48c34b8a6df2c822e5af2e9cf33d5",
+    "url": "/static/js/2.2eb01a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82f7ece268cd202ce9bc",
+    "url": "/static/js/main.b908aa02.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
